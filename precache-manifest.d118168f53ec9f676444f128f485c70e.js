5e20cccd83beb58bc91723f076692327f6772a4b
Simeon <simeoncoding@gmail.com>
2019-03-19 17:20:14 +0000

    Updates

---
+++ precache-manifest.d118168f53ec9f676444f128f485c70e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/test-cra/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "2d73c1f688e3fd06f02e",
+    "url": "/test-cra/static/js/runtime~main.174f6f37.js"
+  },
+  {
+    "revision": "dfac0d9c973e61ddc383",
+    "url": "/test-cra/static/js/main.3ea34fa5.chunk.js"
+  },
+  {
+    "revision": "ba264cd90c4d36546d5a",
+    "url": "/test-cra/static/js/2.8f71a45e.chunk.js"
+  },
+  {
+    "revision": "dfac0d9c973e61ddc383",
+    "url": "/test-cra/static/css/main.f86a6e0b.chunk.css"
+  },
+  {
+    "revision": "a7abcc687924b38c475204715f5db402",
+    "url": "/test-cra/index.html"
+  }
+];
